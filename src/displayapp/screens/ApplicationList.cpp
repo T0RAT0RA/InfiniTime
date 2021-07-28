cbde4b01385c9052f27564658665734dd5c27342
Stéphane Reiss <stephane.reiss@gmail.com>
2021-07-27 23:40:01 -0400

    Test build without modifications

--- src/displayapp/screens/ApplicationList.cpp
+++ src/displayapp/screens/ApplicationList.cpp
@@ -25,9 +25,9 @@ ApplicationList::ApplicationList(Pinetime::Applications::DisplayApp* app,
                [this]() -> std::unique_ptr<Screen> {
                  return CreateScreen2();
                },
-               [this]() -> std::unique_ptr<Screen> {
-                 return CreateScreen3();
-               },
+              //  [this]() -> std::unique_ptr<Screen> {
+              //    return CreateScreen3();
+              //  },
              },
              Screens::ScreenListModes::UpDown} {
 }
@@ -72,14 +72,14 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
   return std::make_unique<Screens::Tile>(1, 2, app, settingsController, batteryController, dateTimeController, applications);
 }
 
-std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
-  std::array<Screens::Tile::Applications, 6> applications {{
-    {"A", Apps::Navigation},
-    {"B", Apps::Clock},
-    {"C", Apps::Music},
-    {"D", Apps::SysInfo},
-    {"E", Apps::BatteryInfo},
-  }};
+// std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
+//   std::array<Screens::Tile::Applications, 6> applications {{
+//     {"A", Apps::Navigation},
+//     {"B", Apps::Clock},
+//     {"C", Apps::Music},
+//     {"D", Apps::SysInfo},
+//     {"E", Apps::BatteryInfo},
+//   }};
 
-  return std::make_unique<Screens::Tile>(2, 3, app, settingsController, batteryController, dateTimeController, applications);
-}
+//   return std::make_unique<Screens::Tile>(2, 3, app, settingsController, batteryController, dateTimeController, applications);
+// }
