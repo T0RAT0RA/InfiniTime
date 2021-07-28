fc2b8e3f2040b72670a81d89cd6f4174eca3b128
Stéphane Reiss <stephane.reiss@gmail.com>
2021-07-27 23:05:36 -0400

    Enable screen 3

--- src/displayapp/screens/ApplicationList.cpp
+++ src/displayapp/screens/ApplicationList.cpp
@@ -25,7 +25,9 @@ ApplicationList::ApplicationList(Pinetime::Applications::DisplayApp* app,
                [this]() -> std::unique_ptr<Screen> {
                  return CreateScreen2();
                },
-               //[this]() -> std::unique_ptr<Screen> { return CreateScreen3(); }
+               [this]() -> std::unique_ptr<Screen> {
+                 return CreateScreen3();
+               },
              },
              Screens::ScreenListModes::UpDown} {
 }
@@ -70,7 +72,7 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
   return std::make_unique<Screens::Tile>(1, 2, app, settingsController, batteryController, dateTimeController, applications);
 }
 
-/*std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
+std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
   std::array<Screens::Tile::Applications, 6> applications {
           {{"A", Apps::Meter},
            {"B", Apps::Navigation},
@@ -82,4 +84,4 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
   };
 
   return std::make_unique<Screens::Tile>(2, 3, app, settingsController, batteryController, dateTimeController, applications);
-}*/
+}
